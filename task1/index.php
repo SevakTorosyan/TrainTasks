cd90fbe2420a05b0c25723d64ebd3acf8fb5c996
Sevak Torosyan <sevak754@gmail.com>
2020-02-27 19:07:20 +0300

    Вторая задача XML + доработки первой

--- task1/index.php
+++ task1/index.php
@@ -1,28 +1,34 @@
 <?php
 DEFINE("ARRIVAL",1);
 DEFINE("DEPARTURE",0);
+
 $input = fopen("input.txt",'r');
 $output = fopen("output.txt", 'w');
-$flightsCount = fgets($input);
-for($i=0; $i<$flightsCount; $i++){
+
+$format = 'd.m.Y_H:i:sT';
+
+$flightsNumber = fgets($input);
+
+for($i=0; $i<$flightsNumber; $i++){
     $currentFlight = fgets($input);
     $unixFlightTime = [];
     $infoAboutFlight = explode(" ", $currentFlight);
     for($j=0;$j<count($infoAboutFlight);$j=$j+2) {
-        $date = DateTime::createFromFormat('d.m.Y_H:i:sT',
-                                             $infoAboutFlight[$j] . parseToStandartForm($infoAboutFlight[$j+1]),
-                                                   new DateTimeZone('UTC'));
-        //echo $date->format("d.m.Y_H:i:sT U") . "<br>";
+        $date = DateTime::createFromFormat($format,
+                                     $infoAboutFlight[$j] . parseToRightForm($infoAboutFlight[$j+1]),
+                                            new DateTimeZone('UTC'));
         array_push($unixFlightTime, $date->format("U"));
     }
     fwrite($output,$unixFlightTime[ARRIVAL]-$unixFlightTime[DEPARTURE]."\n");
 }
+
 fclose($input);
 fclose($output);
+
 echo "<h1>Загляните в файл output.txt</h1>";
 
-function parseToStandartForm($timezone){
-    if ($timezone>0){
+function parseToRightForm($timezone){
+    if ($timezone>=0){
         return "+".(int)$timezone;
     }
     else
